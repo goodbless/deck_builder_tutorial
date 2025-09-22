a35a81ba9fac0043d74d46adf9197bbff065616f
goodbless <evenGodnotknow@hotmail.com>
2025-09-22 13:30:28 +0800

    Add CardPileOpener UI component with dynamic counter for card pile size

---
+++ scenes/ui/card_pile_opener.gd
@@ -0,0 +1,14 @@
+class_name CardPileOpener
+extends TextureButton
+
+@export var counter: Label
+@export var card_pile: CardPile : set = set_card_pile
+
+func set_card_pile(new_value: CardPile) -> void:
+    card_pile = new_value
+
+    if not card_pile.card_pile_size_changed.is_connected(_on_card_pile_size_changed):
+        card_pile.card_pile_size_changed.connect(_on_card_pile_size_changed)
+
+func _on_card_pile_size_changed(new_size: int) -> void:
+    counter.text = str(new_size)
