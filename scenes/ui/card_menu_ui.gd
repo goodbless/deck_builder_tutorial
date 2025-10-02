5ba33f2b8d01e4eb6930e33b024381e681d49eb1
goodbless <evenGodnotknow@hotmail.com>
2025-10-02 15:37:39 +0800

    Refactor card visuals into reusable CardVisuals component
    Extracted card visuals (panel, cost, icon, rarity) into a new CardVisuals scene and script for reuse in card UI and card menu UI. Updated card resource to support rarity and its color mapping. Refactored card UI and card menu UI scripts and scenes to use CardVisuals, improving maintainability and consistency.

--- scenes/ui/card_menu_ui.gd
+++ scenes/ui/card_menu_ui.gd
@@ -8,24 +8,21 @@ const HOVER_STYLEBOX := preload("res://scenes/card_ui/card_hover_stylebox.tres")
 
 @export var card: Card : set = set_card
 
-@onready var panel: Panel = $Visuals/Panel
-@onready var cost: Label = $Visuals/Cost
-@onready var icon: TextureRect = $Visuals/Icon
+@onready var visuals: CardVisuals = $Visuals
 
 func _on_visuals_gui_input(event:InputEvent) -> void:
 	if event.is_action_pressed("left_mouse"):
 		tooltip_requested.emit(card)
 
 func _on_visuals_mouse_entered() -> void:
-	panel.set("theme_override_styles/panel", HOVER_STYLEBOX)
+	visuals.panel.set("theme_override_styles/panel", HOVER_STYLEBOX)
 
 func _on_visuals_mouse_exited() -> void:
-	panel.set("theme_override_styles/panel", BASE_STYLEBOX)
+	visuals.panel.set("theme_override_styles/panel", BASE_STYLEBOX)
 
-func set_card(new_card: Card) -> void:
+func set_card(value: Card) -> void:
 	if not is_node_ready():
 		await ready
 
-	card = new_card
-	cost.text = str(card.cost)
-	icon.texture = card.icon
+	card = value
+	visuals.card = card
