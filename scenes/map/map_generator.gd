16b931ac5fc0b5e5a70dc7533b303c7038059976
goodbless <evenGodnotknow@hotmail.com>
2025-10-13 21:05:36 +0800

    Refactor map generation logic to include random starting points and improve connection setup

--- scenes/map/map_generator.gd
+++ scenes/map/map_generator.gd
@@ -25,10 +25,20 @@ func _ready() -> void:
 
 func generate_map() -> Array[Array]:
     map_data = _generate_initial_grid()
+    var starting_points := _get_random_starting_points()
+
+
+    for j in starting_points:
+        var current_j = j
+        for i in FLOORS - 1:
+            current_j = _setup_connections(i, current_j)
+
 
     var i := 0
     for floor in map_data:
-        print("Floor %s:\t%s" % [i, floor])
+        print("floor %s" % i)
+        var used_rooms = floor.filter(func(r): return r.next_rooms.size() > 0)
+        print(used_rooms)
         i += 1
 
     return []
@@ -57,3 +67,54 @@ func _generate_initial_grid() -> Array[Array]:
         result.append(adjacent_rooms)
 
     return result
+
+func _get_random_starting_points() -> Array[int]:
+    var y_coordinates: Array[int]
+    var unique_points: int = 0
+
+    while unique_points < 2:
+        unique_points = 0
+        y_coordinates = []
+
+        for i in PATHS:
+            var starting_point := randi_range(0, MAP_WIDTH - 1)
+            if not y_coordinates.has(starting_point):
+                unique_points += 1
+
+            y_coordinates.append(starting_point)
+
+    return y_coordinates
+
+func _setup_connections(i: int, j: int) -> int:
+    var next_room: Room
+    var current_room := map_data[i][j] as Room
+
+    while not next_room or _would_cross_existing_path(i, j, next_room):
+        var random_j := clampi(randi_range(j - 1, j + 1), 0, MAP_WIDTH - 1)
+        next_room = map_data[i + 1][random_j]
+
+    current_room.next_rooms.append(next_room)
+
+    return next_room.column
+
+func _would_cross_existing_path(i: int, j: int, room: Room) -> bool:
+    var left_neighbour: Room
+    var right_neighbour: Room
+
+    if j > 0:
+        left_neighbour = map_data[i][j - 1]
+        
+    if j < MAP_WIDTH - 1:
+        right_neighbour = map_data[i][j + 1]
+
+    if right_neighbour and room.column > j:
+        for next_room: Room in right_neighbour.next_rooms:
+            if next_room.column <= room.column:
+                return true
+
+    if left_neighbour and room.column < j:
+        for next_room: Room in left_neighbour.next_rooms:
+            if next_room.column >= room.column:
+                return true
+
+    return false
